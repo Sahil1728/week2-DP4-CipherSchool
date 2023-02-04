7d2bfd674284782c0b55948150e8d5965d45fa66
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-02-04 23:37:24 +0530

    Create 02_topkFrequentElements.cpp

---
+++ 02_topkFrequentElements.cpp
@@ -0,0 +1,30 @@
+// leetcode link of the problem
+// https://leetcode.com/problems/top-k-frequent-elements/
+// submission link: https://leetcode.com/submissions/detail/891482844/
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        unordered_map<int,int> freq;
+        int maxFreq=0;
+        for(int elem:nums){
+            freq[elem]++;
+            maxFreq = max(maxFreq,freq[elem]);
+        }
+        vector<vector<int>> vals(maxFreq+1);
+        for(auto it=freq.begin();it!=freq.end();it++){
+            vals[it->second].push_back(it->first);
+        }
+        int it=maxFreq;
+        while(k>0){
+            if(vals[it].size()==0){
+                it--;
+                continue;
+            }
+            res.push_back(vals[it][vals[it].size()-1]);
+            vals[it].pop_back();
+            k--;
+        }
+        return res;
+    }
+};
